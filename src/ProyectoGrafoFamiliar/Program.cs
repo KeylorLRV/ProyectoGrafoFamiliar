dd5d59ca35bf93228267c641a8c4c5106a44fd38
Keylor Rivera <keyloralej0699@gmail.com>
2025-11-03 20:09:35 -0600

    Inicio Fase 1: Estructura básica y planificación

--- src/ProyectoGrafoFamiliar/Program.cs
+++ src/ProyectoGrafoFamiliar/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProyectoGrafoFamiliar.Datos;
+using ProyectoGrafoFamiliar.Logica;
+using ProyectoGrafoFamiliar.Presentacion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ejemplo inicial para probar integración
+        Persona p = new Persona();
+        GrafoFamiliar grafo = new GrafoFamiliar();
+        MainForm mainForm = new MainForm();
+
+        // Aquí podría iniciar la interfaz gráfica o pruebas de consola
+    }
+}
+
