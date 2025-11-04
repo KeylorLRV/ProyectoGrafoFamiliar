6bd7893fd2ba420c9b180dba66e9a2cd8b973039
Keylor Rivera <keyloralej0699@gmail.com>
2025-11-03 20:19:25 -0600

    Inicio Fase 1: Estructura básica y planificación

---
+++ src/ProyectoGrafoFamiliar/Presentación/MainForm.Designer.cs
@@ -0,0 +1,45 @@
+namespace ProyectoGrafoFamiliar.Presentacion
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.BackColor = System.Drawing.Color.FromArgb(37, 37, 38);
+            this.ForeColor = System.Drawing.Color.FromArgb(204, 204, 204);
+            // 
+            // Form controls collection
+            // 
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+                
+            });
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        // Control declarations
+        
+    }
+}
